6cb575f4ed02a54a5e36479bf276f3db9b08b08b
AndrewR96 <AndrewRivera968@gmail.com>
2016-02-14 22:34:33 -0500

    new files

--- Pods/Target Support Files/Pods-iMovieNet/Pods-iMovieNet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iMovieNet : NSObject
-@end
-@implementation PodsDummy_Pods_iMovieNet
-@end
--- Pods/Target Support Files/Pods-iMovieNet/Pods-iMovieNet-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_iMovieNetVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iMovieNetVersionString[];
-
